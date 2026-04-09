2a88af17e01549310ddb1ef9965f4d9f9d5e3456
SantiagoDiazLopez <santiago.diaz04@usa.edu.co>
2026-04-09 08:11:50 -0500

    Update Examen.java

--- Proyecto_Actividad_4/src/Ejercicio1/Examen.java
+++ Proyecto_Actividad_4/src/Ejercicio1/Examen.java
@@ -1,12 +1,13 @@
 package Ejercicio1;
 
 public class Examen {
-    // Atributos privados para proteger la información del examen
+	
+	// Atributos privados para proteger la información del examen
     private int id;
     private String fecha;
     private String tipo;
     private String resultado;
-
+    
     // Constructor: Inicializa un nuevo objeto Examen con sus datos básicos
     public Examen(int id, String fecha, String tipo, String resultado) {
         this.id = id;
@@ -14,7 +15,7 @@ public class Examen {
         this.tipo = tipo;
         this.resultado = resultado;
     }
-
+    
     // Método toString: Devuelve una cadena de texto formateada con los datos del examen
     public String toString() {
         return "ID: " + id +
@@ -23,3 +24,4 @@ public class Examen {
                ", Resultado: " + resultado;
     }
 }
+
